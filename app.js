ca83eb0f875c1ca6eeeaf2617911446daf0deac3
Rachana R Shankar <rachanarshnkar@gmail.com>
2021-01-08 12:56:34 +0530

    First Change

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/bankApp', {
+mongoose.connect(process.env.DATABASEURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
     })
